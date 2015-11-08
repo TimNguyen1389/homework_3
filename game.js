b4d1f969c8bb69a6c0b2fcc361ec7498ad23b7ad
Tim Nguyen <tim.thien@yahoo.com>
2015-11-07 19:40:50 -0800

    add while loops, input validation, and move greetings alert into DOM

--- game.js
+++ game.js
@@ -10,13 +10,24 @@ var myNum2 = 10;
 var tallyStr = 'Intense computing... Gears whirling... Gerbils fatiguing...'
 
 //Next we meet our user and prep them for what will follow.
-
 var name = prompt("What is your name?");
-alert("It's nice to meet you " + name + ". My name is Michael. Try to answer the following questions to learn more about me.");
+  //use while loop to check for empty value/string.
+  while (name === "") {
+    alert("Oops! You forgot to enter your name.");
+    name = prompt("What is your name?");
+  }
+
+document.getElementById('greetings').innerHTML = ("It's nice to meet you " + name + ". My name is Michael. Try to answer the following questions to learn more about me.");
 
 //question time!
 function q1() {
 	var a1 = prompt("Did I graduate college? Please answer by typing either 'yes' or 'no'").substring(0,1).toLowerCase();
+
+  //use while loop to check for yes or no answer only.
+  while (a1.substring(0,1).toLowerCase() != "y" && a1.substring(0,1).toLowerCase() != "n") {
+    alert("Invalid answer! Yes or no answer only please.");
+    a1 = prompt("Did I graduate college? Please answer by typing either 'yes' or 'no'");
+  }
 
 	/*I found that I can get by with only 2 possible affirmative conditions if I first convert the user input to all lowercase as seen above.
 	Additionally, I found that by cutting the string down to only take the first letter of their input I can account for answers like "yup" or
@@ -39,6 +50,8 @@ q1();
 /*I left this second question to represent what I originally had as it illustrates the difference in code from the first.
 The big drawback here is that by not changing the input to all one case we would have to provide additional statements for things like "YEs" or "yES"
 in order for the code to have the same functionality as the first question*/
+
+//Did not add a while loop here because Navigator wanted to make a point.
 function q2() {
 	var a2 = prompt("Do I have a pet? (y/n)");
 
@@ -59,9 +72,16 @@ q2();
 /*If I was a good programmer with more time on my hands I would create a condition that only allowed for the user to input acceptable responses to our questions.
 Non-sense input like "kajsfdhkasjhf" or irrelevant input like "cat" would loop back to the original question until some form of yes or no was entered instead
 of just being recorded as a "no" answer.*/
+
+// addressed above concern by adding a while loop.
 function q3() {
 	var a3 = prompt("Have I traveled outside of North America? (y/n)").substring(0,1).toLowerCase();
 
+	  //use while loop to check for yes or no answer only.
+  while (a3.substring(0,1).toLowerCase() != "y" && a3.substring(0,1).toLowerCase() != "n") {
+    alert("Invalid answer! Yes or no answer only please.");
+    a3 = prompt("Have I traveled outside of North America? (y/n)");
+  }
 		if ((a3 === "y") ||  (a3 === "yes"))
 		{
 			document.getElementById('a3').innerHTML = (ya + ' I have been outside North America. To Japan to be exact. And full disclosure, I lied about my age so that I could drink alcohol. =)');
@@ -78,15 +98,21 @@ q3();
 method combined with a series of if, else-if statements that examine our user input and compare to our "ideal number"
 that we declared in variable myNum1 at the beginning of our code. */
 function q4() {
-	var a4 = parseInt(prompt('I am thinking of a number between 1 and 10. Input your guess in the prompt below'));
-		if(a4 === myNum1){
+	var a4 = prompt('I am thinking of a number between 1 and 10. Input your guess in the prompt below');
+	while (isNaN(a4)) {
+		//alert if user's answer is not a number.
+	  alert(a4 + " is not a number!");
+		a4 = prompt('I am thinking of a number between 1 and 10. Input your guess in the prompt below');
+		}
+
+		if(parseInt(a4, 10) === myNum1){
 			document.getElementById('a4').innerHTML = ('Nice! You guessed my number! Come buy me a lottery ticket ' + name + '.');
 			numberCorrect++;
 		}
-		else if (a4 > myNum1){
+		else if (parseInt(a4, 10) > myNum1){
 			document.getElementById('a4').innerHTML = ('Sorry your guess was too high');
 		}
-		else if (a4 < myNum1){
+		else if (parseInt(a4, 10) < myNum1){
 			document.getElementById('a4').innerHTML = ('Sorry your guess was too low');
 		}
 	}
@@ -94,6 +120,11 @@ q4();
 
 function q5() {
 	var a5 = prompt("Have I been skydiving? (y/n)").substring(0,1).toLowerCase();
+	  //use while loop to check for yes or no answer only.
+  while (a5.substring(0,1).toLowerCase() != "y" && a5.substring(0,1).toLowerCase() != "n") {
+    alert("Invalid answer! Yes or no answer only please.");
+    a5 = prompt("Have I been skydiving? (y/n)");
+  }
 
 		if (a5 === "y")
 		{
@@ -108,15 +139,22 @@ function q5() {
 q5();
 
 function q6(){
-	var a6 = parseInt(prompt('I am thinking of a new number between 1 and 10. Input your guess in the prompt below'));
-		if(a6 === myNum2){
+	var a6 = prompt('I am thinking of a new number between 1 and 10. Input your guess in the prompt below');
+
+	while (isNaN(a6)) {
+		//alert if user's answer is not a number.
+	  alert(a6 + " is not a number!");
+		a6 = prompt('I am thinking of a new number between 1 and 10. Input your guess in the prompt below');
+		}
+
+		if(parseInt(a6, 10) === myNum2){
 			document.getElementById('a6').innerHTML = ('Nice! You guessed my number! Come buy me a lottery ticket ' + name + '.');
 			numberCorrect++;
 		}
-		else if (a6 > myNum2){
+		else if (parseInt(a6, 10) > myNum2){
 			document.getElementById('a6').innerHTML = ('Sorry your guess was too high. Better luck next time.');
 		}
-		else if (a6 < myNum2){
+		else if (parseInt(a6, 10) < myNum2){
 			document.getElementById('a6').innerHTML = ('Sorry your guess was too low. Better luck next time.');
 		}
 	}
